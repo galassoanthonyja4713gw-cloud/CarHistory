a894f97694b9ab98cda49e1425a820c149991041
Anthony <galassoanthony.ja4713gw@gmail.com>
2026-01-08 14:10:05 -0700

    update dbping for testing

--- CarHistory.Api/Functions/DbPingFunction.cs
+++ CarHistory.Api/Functions/DbPingFunction.cs
@@ -1,34 +1,14 @@
-﻿using CarHistory.Api.Data;
-using Microsoft.Azure.Functions.Worker;
+﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.EntityFrameworkCore;
-using System.Net;
 
 public class DbPingFunction
 {
-    private readonly AppDbContext _db;
-
-    public DbPingFunction(AppDbContext db) => _db = db;
-
-    [Function("db-ping")]
-    public async Task<HttpResponseData> Run(
+    [Function("DbPing")]
+    public HttpResponseData Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "db-ping")] HttpRequestData req)
     {
-        try
-        {
-            // simplest possible DB touch
-            await _db.Database.OpenConnectionAsync();
-            await _db.Database.CloseConnectionAsync();
-
-            var ok = req.CreateResponse(HttpStatusCode.OK);
-            ok.WriteString("db ok");
-            return ok;
-        }
-        catch (Exception ex)
-        {
-            var fail = req.CreateResponse(HttpStatusCode.InternalServerError);
-            fail.WriteString(ex.ToString()); // TEMP: remove after debugging
-            return fail;
-        }
+        var res = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        res.WriteString("function alive");
+        return res;
     }
 }
