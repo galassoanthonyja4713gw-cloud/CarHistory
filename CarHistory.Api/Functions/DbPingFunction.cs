1f006dc7d9b29d88a5c4db050f23fe3aadb81d59
Anthony <galassoanthony.ja4713gw@gmail.com>
2026-01-08 14:32:35 -0700

    update dbping again

--- CarHistory.Api/Functions/DbPingFunction.cs
+++ CarHistory.Api/Functions/DbPingFunction.cs
@@ -1,15 +1,41 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿using CarHistory.Api.Data;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Net;
+using System.Threading.Tasks;
 
-public class DbPingFunction
+namespace CarHistory.Api.Functions
 {
-    [Function("Ping")]
-    public HttpResponseData Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")] HttpRequestData req)
+    public class DbPingFunction
     {
-        var res = req.CreateResponse(HttpStatusCode.OK);
-        res.WriteString("pong");
-        return res;
+        private readonly AppDbContext _db;
+
+        public DbPingFunction(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [Function("DbPing")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "db-ping")] HttpRequestData req)
+        {
+            try
+            {
+                await _db.Database.OpenConnectionAsync();
+                await _db.Database.CloseConnectionAsync();
+
+                var ok = req.CreateResponse(HttpStatusCode.OK);
+                await ok.WriteStringAsync("db ok");
+                return ok;
+            }
+            catch (Exception ex)
+            {
+                var fail = req.CreateResponse(HttpStatusCode.InternalServerError);
+                await fail.WriteStringAsync(ex.ToString());
+                return fail;
+            }
+        }
     }
 }
