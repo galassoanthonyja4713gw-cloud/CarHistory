c12021f0fe7b4b568ab6d3141dacf2e47af10e80
Anthony <galassoanthony.ja4713gw@gmail.com>
2026-01-08 15:10:02 -0700

    fix config

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    base: "/", // IMPORTANT for Azure Static Web Apps
     server: {
         proxy: {
             "/api": {
