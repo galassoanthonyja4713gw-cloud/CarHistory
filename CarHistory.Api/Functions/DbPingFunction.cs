246439277ee22500262b68e87ab7b477c387b1ba
Anthony <galassoanthony.ja4713gw@gmail.com>
2026-01-08 14:13:23 -0700

    add new ping function

--- CarHistory.Api/Functions/DbPingFunction.cs
+++ CarHistory.Api/Functions/DbPingFunction.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
 
 public class DbPingFunction
 {
@@ -10,5 +11,18 @@ public class DbPingFunction
         var res = req.CreateResponse(System.Net.HttpStatusCode.OK);
         res.WriteString("function alive");
         return res;
+    }
+
+    public class PingFunction
+    {
+        [Function("Ping")]
+        public HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")] HttpRequestData req)
+        {
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            res.Headers.Add("content-type", "text/plain; charset=utf-8");
+            res.WriteString("pong-2026-01-08");
+            return res;
+        }
     }
 }
